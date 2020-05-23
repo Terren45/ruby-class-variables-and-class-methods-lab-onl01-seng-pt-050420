95608fa6f57c434a0b1534aa5312454946574d25
Terren Culberson <culbersonterren@yahoo.com>
2020-05-23 03:48:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   
   def self.genre_count
     genres = {}
-    genres.each do |genre|
+    @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += l 
       else
