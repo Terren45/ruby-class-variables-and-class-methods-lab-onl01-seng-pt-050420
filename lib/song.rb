e618321f719387d4f6eb8dbccfe498a7d15771bc
Terren Culberson <culbersonterren@yahoo.com>
2020-05-23 04:00:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,8 +39,8 @@ def self.genre_count
   end
 end
    
-   def self.artists_count
-    artist_count = {}
+   def self.artist_count
+    artist_counts = {}
     @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1 
