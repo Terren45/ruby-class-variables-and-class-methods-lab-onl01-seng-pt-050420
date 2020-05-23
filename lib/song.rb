03c126cd0ba5b1aa02c82121523dc1d502655f75
Terren Culberson <culbersonterren@yahoo.com>
2020-05-23 03:32:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,10 @@ class Song
   @@artists = []
   
   def initialize(name, artist, genre)
+     
      @name = name
      @artist = artist
      @genre = genre
-     @@count +=1
      @@genres << genre
      @@artists << artist
   end
